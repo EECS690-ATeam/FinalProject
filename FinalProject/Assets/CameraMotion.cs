4b3223a2894515b7219b6213030c5d64fbcd5cdb
Nick Velicer <njvelicer@ku.edu>
2023-03-07 20:11:56 -0600

    Adding design textures and color gradient

---
+++ FinalProject/Assets/CameraMotion.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour
+{
+    public Transform player;
+    public float smoothTime = 0.1f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
+
