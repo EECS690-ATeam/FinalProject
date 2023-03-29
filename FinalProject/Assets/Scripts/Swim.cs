97a063dd7eb92f823e61c5a5c734714f9a8551c9
jtsizoo <56416723+jtsizoo@users.noreply.github.com>
2023-03-29 12:40:56 -0500

    Update swim animations/movement

--- FinalProject/Assets/Scripts/Swim.cs
+++ FinalProject/Assets/Scripts/Swim.cs
@@ -11,54 +11,74 @@ public class Swim : MonoBehaviour
     private SpriteRenderer sr;
     public float verticalRot = 30;
     public float horizontalRot = 180;
-
-
-    bool dec = false;
     bool inc = false;
+    bool dec = false;
+
+    bool orientation = false; //false = vertical -> true = horizontal
+
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        animator.SetBool("IsIdle", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(orientation);
         var horizontalMovement = Input.GetAxis("Horizontal");
         var verticalMovement = Input.GetAxis("Vertical");
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
-        animator.SetFloat("Speed1", Mathf.Abs(verticalMovement));
+        if(horizontalMovement != 0) animator.SetBool("IsIdle", false);
+        else animator.SetBool("IsIdle", true);
         transform.position += new Vector3(horizontalMovement, verticalMovement, 0) * Time.deltaTime * MovementSpeed;
 
-        if(!(GameObject.eulerAngles.z >90 && GameObject.eulerAngles.z < 270)) {
-            transform.Rotate(new Vector3(0,0,verticalMovement) * Time.deltaTime * verticalRot);
-            if(GameObject.eulerAngles.z > 90 && GameObject.eulerAngles.z < 92) dec = true;
-            if(GameObject.eulerAngles.z  < 270 && GameObject.eulerAngles.z>268) inc = true;
+
+        if(!orientation && horizontalMovement!=0) { 
+            if((GameObject.eulerAngles.z>270 && GameObject.eulerAngles.z<=360)) {
+                if(facingRight) transform.Rotate(new Vector3(0,0,-horizontalMovement)); //Right angle
+                else transform.Rotate(new Vector3(0,0,-1));
+            }
+            if(GameObject.eulerAngles.z<270 && GameObject.eulerAngles.z > 90) {
+                orientation = true;
+            }
+        }
+        else if(horizontalMovement==0) {
+            if(GameObject.eulerAngles.z>220 && GameObject.eulerAngles.z<359) {
+                transform.Rotate(new Vector3(0,0,.25f));
+            }
         }
 
-         if(inc) 
-         {
-            transform.Rotate(new Vector3(0,0,2f));
-            inc = false;
-         }
-         if(dec) {
-            transform.Rotate(new Vector3(0,0,-2f));
-            dec = false;
-         }
+        if(orientation) {
+            if(GameObject.eulerAngles.z>225 && GameObject.eulerAngles.z<320) {
+                transform.Rotate(new Vector3(0,0,verticalMovement));
+            }
+            if(GameObject.eulerAngles.z<225) inc = true;
+            if(GameObject.eulerAngles.z>320) {
+                if(horizontalMovement==0) orientation = false;
+                animator.SetBool("IsIdle", true);
+            }
+        }
 
+        if(inc) {
+            transform.Rotate(new Vector3(0,0,2));
+            inc = false;
+        }
+        if(dec) {
+            transform.Rotate(new Vector3(0,0,2));
+            dec = false;
+        }
+
+        //Y Axis 180 Rotation when switching sides
         if(transform.rotation.y >0 && transform.rotation.y < 180 && horizontalMovement < 0) {
-            transform.Rotate(new Vector3(0,Time.deltaTime,0) * horizontalRot*2);
-            //if(GameObject.eulerAngles.z<180) transform.Rotate(new Vector3(0,0,180-GameObject.eulerAngles.z));
-            //if(GameObject.eulerAngles.z>180) transform.Rotate(new Vector3(0,0,360-GameObject.eulerAngles.z));
+            transform.Rotate(new Vector3(0,Time.deltaTime,0) * horizontalRot*2, Space.World);
         }
         if(horizontalMovement<0 && facingRight) {
             facingRight = !facingRight;
             transform.Rotate(new Vector3(0,Time.deltaTime,0) * horizontalRot);
         }
         if(transform.rotation.y > -180 && transform.rotation.y < 0 && horizontalMovement >0) {
-            transform.Rotate(new Vector3(0,Time.deltaTime,0)*horizontalRot*2);
-            //if(GameObject.eulerAngles.z<180) transform.Rotate(new Vector3(0,0,180-GameObject.eulerAngles.z));
-            //if(GameObject.eulerAngles.z>180) transform.Rotate(new Vector3(0,0,360-GameObject.eulerAngles.z));
+            transform.Rotate(new Vector3(0,Time.deltaTime,0)*horizontalRot*2, Space.World);
         }
         if(horizontalMovement>0 && !facingRight) {
             facingRight = !facingRight;
