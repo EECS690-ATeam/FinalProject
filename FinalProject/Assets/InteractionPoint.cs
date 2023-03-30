097c0b218f94f92840c4ae015679f2cafaf6e915
Nick Velicer <njvelicer@ku.edu>
2023-03-29 23:04:39 -0500

    Interaction point prototype

---
+++ FinalProject/Assets/InteractionPoint.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InteractionPoint : MonoBehaviour
+{
+    public string message;
+    public TMP_Text thought;
+    private Player p;
+    private bool beenSeen;
+    private int distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = FindObjectOfType<Player>();
+        beenSeen = false;
+        distance = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e") && !beenSeen) {
+            float diff = Vector3.Distance(this.transform.position, p.transform.position);
+            if (diff < distance) {
+                this.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                this.gameObject.transform.GetChild(1).GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+                beenSeen = true;
+                StartCoroutine(adjustMessage());
+            }
+        }
+
+    }
+
+    IEnumerator adjustMessage() {
+        thought.text = message;
+        yield return new WaitForSeconds(5);
+        thought.text = "";
+    }
+}
