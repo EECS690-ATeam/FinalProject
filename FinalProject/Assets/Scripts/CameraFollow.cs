fb5d3efd8b0e16ce5b70e2b6e6d8601fe8c9dbac
Ryan Penrod <ryanmpenrod@gmail.com>
2023-04-07 18:52:12 -0500

    Camera Sway

--- FinalProject/Assets/Scripts/CameraFollow.cs
+++ FinalProject/Assets/Scripts/CameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,12 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform player;
     public float smoothTime = 0.1f;
+    public float swayAmplitude = 1f;
+    public float swayFrequency = 0.5f;
 
     private Vector3 velocity = Vector3.zero;
+    private float currentSway;
+    private Vector3 swayVector;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +23,15 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player != null)
+
+        if (player != null)
         {
-            Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+            currentSway = (float)Mathf.Sin(Time.time * swayFrequency) * swayAmplitude;
+
+            Debug.Log(currentSway);
+
+            Vector3 targetPosition = new Vector3(player.position.x + currentSway, player.position.y, transform.position.z);
+
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
     }
