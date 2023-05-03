332268faeee170e294659b9b61d78dd27f02e94c
jtsizoo <56416723+jtsizoo@users.noreply.github.com>
2023-05-02 20:00:35 -0500

    minor swim fixes

--- FinalProject/Assets/ResetCancelTurn.cs
+++ FinalProject/Assets/ResetCancelTurn.cs
@@ -2,24 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResetTurnHead : StateMachineBehaviour
+public class ResetCancelTurn : StateMachineBehaviour
 {
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       animator.SetBool("CancelTurn", false);
+    }
+
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
     //    
     //}
 
-    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       animator.SetBool("HeadTurn", false);
+       animator.SetBool("CancelTurn", false);
     }
-
-    // override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    // {
-    //    animator.SetBool("HeadTurn", false);
-    // }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
